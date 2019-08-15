1c393e845a35adc6ca3b711db030b2ed2ca95467
Jean Carlos <jeancarlos_maia@hotmail.com>
2019-08-15 20:59:10 -0300

    seller insert and update

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -10,11 +13,12 @@ import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 
 			
 			GenericDao<Seller> sellerDao = DaoFactory.createSellerDao();
+			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 			Seller seller = sellerDao.find(2);
 			System.out.println(seller);
 			
@@ -26,7 +30,17 @@ public class Program {
 			System.out.println();
 			list = sellerDao.searchAll();
 			list.forEach(System.out::println);
-
+			//////////////////////////////////////
+			//System.out.println();
+			//Seller s = new Seller(3, "Felipe Freitas Maia", "freitasmaiafelipe@gmail.com", sdf.parse("12/10/2003"), 5000.00, new Department(2, null));
+			//sellerDao.insert(s);
+			//////////////////////////////////////
+			System.out.println();
+			seller = sellerDao.find(13);
+			seller.setName("Not Anakin");
+			seller.setEmail("notthefather@gmail.com");
+			seller.setBaseSalary(1000.00);
+			System.out.println(seller);
 
 	}
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -24,15 +25,56 @@ public class SellerDaoJDBC implements GenericDao<Seller> {
 	}
 
 	@Override
-	public void insert(Seller t) {
-		// TODO Auto-generated method stub
+	public void insert(Seller s) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("INSERT INTO seller " + 
+					"(Name, Email, BirthDate, BaseSalary, DepartmentId)  "
+					+ "VALUES  (?, ?, ?, ?, ?) ",
+					Statement.RETURN_GENERATED_KEYS);
 
+			st.setString(1, s.getName());
+			st.setString(2, s.getEmail());
+			st.setDate(3, new java.sql.Date(s.getBirthDate().getTime()));
+			st.setDouble(4, s.getBaseSalary());
+			st.setInt(5, s.getDepartment().getId());
+
+			st.executeUpdate();
+
+			ResultSet rs = st.getGeneratedKeys();
+			rs.next();
+			s.setId(rs.getInt(1));
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
 	}
 
 	@Override
-	public void update(Seller t) {
-		// TODO Auto-generated method stub
+	public void update(Seller s) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("UPDATE seller  "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, "
+					+ "BaseSalary = ?, DepartmentId = ?  "
+					+ "WHERE Id = ? ");
 
+			st.setString(1, s.getName());
+			st.setString(2, s.getEmail());
+			st.setDate(3, new java.sql.Date(s.getBirthDate().getTime()));
+			st.setDouble(4, s.getBaseSalary());
+			st.setInt(5, s.getDepartment().getId());
+			st.setInt(6, s.getId());
+			
+			st.executeUpdate();
+			
+			DB.closeStatement(st);
+
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
 	}
 
 	@Override
@@ -89,22 +131,19 @@ public class SellerDaoJDBC implements GenericDao<Seller> {
 		ResultSet rs = null;
 		try {
 			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName  "
-					+ "FROM seller INNER JOIN department  "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "ORDER BY Name");
-			
-			
+					"SELECT seller.*,department.Name as DepName  " + "FROM seller INNER JOIN department  "
+							+ "ON seller.DepartmentId = department.Id " + "ORDER BY Name");
+
 			rs = st.executeQuery();
-			
+
 			List<Seller> list = new ArrayList<>();
-			
+
 			Map<Integer, Department> map = new HashMap<>();
-			
+
 			Seller s = new Seller();
-			
+
 			while (rs.next()) {
-				
+
 				Department dep = map.get(rs.getInt("DepartmentId"));
 				if (dep == null) {
 					dep = instantiateDep(rs);
@@ -114,12 +153,10 @@ public class SellerDaoJDBC implements GenericDao<Seller> {
 				list.add(s);
 			}
 			return list;
-			
-		} 
-		catch (SQLException e) {
+
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		} 
-		finally {
+		} finally {
 			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 		}
@@ -131,24 +168,21 @@ public class SellerDaoJDBC implements GenericDao<Seller> {
 		ResultSet rs = null;
 		try {
 			st = conn.prepareStatement(
-					"SELECT seller.*,department.Name as DepName  "
-					+ "FROM seller INNER JOIN department  "
-					+ "ON seller.DepartmentId = department.Id "
-					+ "WHERE DepartmentId = ? "
-					+ "ORDER BY Name");
-			
+					"SELECT seller.*,department.Name as DepName  " + "FROM seller INNER JOIN department  "
+							+ "ON seller.DepartmentId = department.Id " + "WHERE DepartmentId = ? " + "ORDER BY Name");
+
 			st.setInt(1, d.getId());
-			
+
 			rs = st.executeQuery();
-			
+
 			List<Seller> list = new ArrayList<>();
-			
+
 			Map<Integer, Department> map = new HashMap<>();
-			
+
 			Seller s = new Seller();
-			
+
 			while (rs.next()) {
-				
+
 				Department dep = map.get(rs.getInt("DepartmentId"));
 				if (dep == null) {
 					dep = instantiateDep(rs);
@@ -158,12 +192,10 @@ public class SellerDaoJDBC implements GenericDao<Seller> {
 				list.add(s);
 			}
 			return list;
-			
-		} 
-		catch (SQLException e) {
+
+		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
-		} 
-		finally {
+		} finally {
 			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 		}
